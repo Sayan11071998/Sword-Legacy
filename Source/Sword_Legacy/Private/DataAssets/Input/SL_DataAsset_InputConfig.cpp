0f28976bd453d43d04c76f0d31ace7cfa6e80291
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-22 02:17:25 +0530

    Input Data Asset Created

---
+++ Source/Sword_Legacy/Private/DataAssets/Input/SL_DataAsset_InputConfig.cpp
@@ -0,0 +1,14 @@
+#include "DataAssets/Input/SL_DataAsset_InputConfig.h"
+
+TObjectPtr<UInputAction> USL_DataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag)
+{
+	for (const FSL_InputActionConfig InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	
+	return nullptr;
+}
---
+++ Source/Sword_Legacy/Public/DataAssets/Input/SL_DataAsset_InputConfig.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "SL_DataAsset_InputConfig.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+USTRUCT(BlueprintType)
+struct FSL_InputActionConfig
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> InputAction;
+};
+
+UCLASS()
+class SWORD_LEGACY_API USL_DataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FSL_InputActionConfig> NativeInputActions;
+	
+	TObjectPtr<UInputAction> FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+};
