cbf14260e0fdf97228233d6236fa8fa62619bddd
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-22 01:02:54 +0530

    Base Game Mode Created

---
+++ Source/Sword_Legacy/Private/GameModes/SL_GameModeBase.cpp
@@ -0,0 +1,1 @@
+#include "GameModes/SL_GameModeBase.h"
---
+++ Source/Sword_Legacy/Public/GameModes/SL_GameModeBase.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SL_GameModeBase.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API ASL_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
