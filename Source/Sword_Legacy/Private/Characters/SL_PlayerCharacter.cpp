25dfef9134f5154ffe39280fc19d35eed32d2111
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-22 01:41:29 +0530

    Character Camera Setup Created

--- Source/Sword_Legacy/Private/Characters/SL_PlayerCharacter.cpp
+++ Source/Sword_Legacy/Private/Characters/SL_PlayerCharacter.cpp
@@ -1,1 +1,38 @@
 #include "Characters/SL_PlayerCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+#include "SL_DebugHelper.h"
+
+ASL_PlayerCharacter::ASL_PlayerCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f);
+	CameraBoom->bUsePawnControlRotation = true;
+	
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+	
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+}
+
+void ASL_PlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Debug::Print(TEXT("Init"));
+}
--- Source/Sword_Legacy/Public/Characters/SL_PlayerCharacter.h
+++ Source/Sword_Legacy/Public/Characters/SL_PlayerCharacter.h
@@ -4,8 +4,26 @@
 #include "Characters/SL_BaseCharacter.h"
 #include "SL_PlayerCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class SWORD_LEGACY_API ASL_PlayerCharacter : public ASL_BaseCharacter
 {
 	GENERATED_BODY()
+	
+public:
+	ASL_PlayerCharacter();
+	
+	virtual void BeginPlay() override;
+	
+private:
+#pragma region Components
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> FollowCamera;
+#pragma endregion
 };
