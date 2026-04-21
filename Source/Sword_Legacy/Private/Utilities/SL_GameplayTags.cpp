a48a2cb0e8d77e0d776949007eeb4dde27195785
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-22 02:00:11 +0530

    Input Gameplay Tag Created

---
+++ Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
@@ -0,0 +1,7 @@
+#include "Utilities/SL_GameplayTags.h"
+
+namespace SL_GameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+}
