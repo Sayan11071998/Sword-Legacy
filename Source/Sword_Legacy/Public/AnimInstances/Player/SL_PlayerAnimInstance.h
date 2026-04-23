dcd3a22a0cabcd0b352209db0d4679ec517f9eb5
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-24 00:39:15 +0530

    Idle Relax State Implemented

--- Source/Sword_Legacy/Public/AnimInstances/Player/SL_PlayerAnimInstance.h
+++ Source/Sword_Legacy/Public/AnimInstances/Player/SL_PlayerAnimInstance.h
@@ -4,8 +4,28 @@
 #include "AnimInstances/SL_CharacterAnimInstance.h"
 #include "SL_PlayerAnimInstance.generated.h"
 
+class ASL_PlayerCharacter;
+
 UCLASS()
 class SWORD_LEGACY_API USL_PlayerAnimInstance : public USL_CharacterAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	// ~ Begin UAnimInstance interface
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+	// ~ End UAnimInstance interface
+	
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	TObjectPtr<ASL_PlayerCharacter> OwningPlayerCharacter;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+	
+	float IdleElapsedTime;
 };
