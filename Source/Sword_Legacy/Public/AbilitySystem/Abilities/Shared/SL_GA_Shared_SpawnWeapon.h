bda2de91e701bea8135211230af5cd43d7ae5745
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-25 00:55:44 +0530

    Spawn Weapon Ability Created in C++

---
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Shared/SL_GA_Shared_SpawnWeapon.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/SL_GameplayAbility.h"
+#include "SL_GA_Shared_SpawnWeapon.generated.h"
+
+class ASL_WeaponBase;
+
+UCLASS()
+class SWORD_LEGACY_API USL_GA_Shared_SpawnWeapon : public USL_GameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	// ~ Begin UGameplayAbility Interface
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData	
+	) override;
+	// ~ End UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawn")
+	TSubclassOf<ASL_WeaponBase> WeaponClassToSpawn;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawn")
+	FName SocketNameToAttachTo;
+};
