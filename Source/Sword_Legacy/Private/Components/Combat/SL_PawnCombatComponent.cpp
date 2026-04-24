ba11fc34ef2e2dcdd2948514fa0e334458b0ed64
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-25 02:33:16 +0530

    Register Weapon via Pawn Combat Component

--- Source/Sword_Legacy/Private/Components/Combat/SL_PawnCombatComponent.cpp
+++ Source/Sword_Legacy/Private/Components/Combat/SL_PawnCombatComponent.cpp
@@ -1,1 +1,35 @@
 #include "Components/Combat/SL_PawnCombatComponent.h"
+
+void USL_PawnCombatComponent::RegisterSpawnedWeapon(FGameplayTag InWeaponTagToRegister,
+	ASL_WeaponBase* InWeaponToRegister, bool bRegisterAsEquippedWeapon)
+{
+	checkf(!CharacterCarriedWeaponMap.Contains(InWeaponTagToRegister), TEXT("A tag named %s has already been added as carried weapon"), *InWeaponTagToRegister.ToString());
+	check(InWeaponToRegister);
+	
+	CharacterCarriedWeaponMap.Emplace(InWeaponTagToRegister, InWeaponToRegister);
+	
+	if (bRegisterAsEquippedWeapon)
+	{
+		CurrentEquippedWeaponTag = InWeaponTagToRegister;
+	}
+}
+
+ASL_WeaponBase* USL_PawnCombatComponent::GetCharacterCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const
+{
+	if (CharacterCarriedWeaponMap.Contains(InWeaponTagToGet))
+	{
+		if (ASL_WeaponBase* const* FoundWeapon = CharacterCarriedWeaponMap.Find(InWeaponTagToGet))
+		{
+			return *FoundWeapon;
+		}
+	}
+	
+	return nullptr;
+}
+
+ASL_WeaponBase* USL_PawnCombatComponent::GetCharacterCurrentEquippedWeapon() const
+{
+	if (!CurrentEquippedWeaponTag.IsValid()) return nullptr;
+	
+	return GetCharacterCarriedWeaponByTag(CurrentEquippedWeaponTag);
+}
