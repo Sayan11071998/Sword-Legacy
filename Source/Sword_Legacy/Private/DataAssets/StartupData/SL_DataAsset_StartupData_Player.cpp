f51afc1b79ecb554371cc2a98ea9acf6bc84c62e
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-25 20:32:20 +0530

    Granting Ability To Ability Component

--- Source/Sword_Legacy/Private/DataAssets/StartupData/SL_DataAsset_StartupData_Player.cpp
+++ Source/Sword_Legacy/Private/DataAssets/StartupData/SL_DataAsset_StartupData_Player.cpp
@@ -1,1 +1,26 @@
 #include "DataAssets/StartupData/SL_DataAsset_StartupData_Player.h"
+#include "AbilitySystem/Abilities/SL_GameplayAbility.h"
+#include "AbilitySystem/SL_AbilitySystemComponent.h"
+
+bool FSL_PlayerAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void USL_DataAsset_StartupData_Player::GiveToAbilitySystemComponent(TObjectPtr<USL_AbilitySystemComponent> InASCToGive,
+	int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+	
+	for (const FSL_PlayerAbilitySet& AbilitySet : PlayerStartupAbilitySets)
+	{
+		if (!AbilitySet.IsValid()) continue;
+		
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+		
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
--- Source/Sword_Legacy/Public/DataAssets/StartupData/SL_DataAsset_StartupData_Player.h
+++ Source/Sword_Legacy/Public/DataAssets/StartupData/SL_DataAsset_StartupData_Player.h
@@ -1,11 +1,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "DataAssets/StartupData/SL_DataAsset_StartupData_Base.h"
 #include "SL_DataAsset_StartupData_Player.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSL_PlayerAbilitySet
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<USL_GameplayAbility> AbilityToGrant;
+	
+	bool IsValid() const;
+};
 
 UCLASS()
 class SWORD_LEGACY_API USL_DataAsset_StartupData_Player : public USL_DataAsset_StartupData_Base
 {
 	GENERATED_BODY()
+	
+public:
+	// ~ Begin USL_DataAsset_StartupData_Base Interface
+	virtual void GiveToAbilitySystemComponent(TObjectPtr<USL_AbilitySystemComponent> InASCToGive, int32 ApplyLevel = 1) override;
+	// ~ End USL_DataAsset_StartupData_Base Interface
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Startup Data", meta = (Categories = "InputTag"))
+	TArray<FSL_PlayerAbilitySet> PlayerStartupAbilitySets;
 };
