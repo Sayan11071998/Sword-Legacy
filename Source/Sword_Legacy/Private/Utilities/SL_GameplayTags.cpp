06fe1dc9dff10c38bc29c7210f3294fe1eb5f010
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-25 20:12:03 +0530

    Ability Input Action Bind Method Created

--- Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
+++ Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
@@ -6,6 +6,8 @@ namespace SL_GameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Sprint, "InputTag.Sprint");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_EquipKatana, "InputTag.EquipKatana");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipKatana, "InputTag.UnequipKatana");
 	
 	/** Player Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Katana, "Player.Weapon.Katana");
