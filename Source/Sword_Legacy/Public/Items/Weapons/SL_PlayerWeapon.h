5ed428be4eb9a2ecf365f354530b5a219452b389
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-26 21:55:58 +0530

    Working on Unequipping

--- Source/Sword_Legacy/Public/Items/Weapons/SL_PlayerWeapon.h
+++ Source/Sword_Legacy/Public/Items/Weapons/SL_PlayerWeapon.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "Items/Weapons/SL_WeaponBase.h"
 #include "PawnTypes/SL_PawnStructTypes.h"
+#include "GameplayAbilitySpecHandle.h"
 #include "SL_PlayerWeapon.generated.h"
 
 UCLASS()
@@ -13,4 +14,13 @@ class SWORD_LEGACY_API ASL_PlayerWeapon : public ASL_WeaponBase
 public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WeaponData")
 	FSL_PlayerWeaponData PlayerWeaponData;
+	
+	UFUNCTION(BlueprintCallable)
+	void AssignGrantedAbilitySpecHandles(const TArray<FGameplayAbilitySpecHandle>& InSpecHandles);
+	
+	UFUNCTION(BlueprintPure)
+	TArray<FGameplayAbilitySpecHandle>  GetGrantedAbilitySpecHandles() const;
+	
+private:
+	TArray<FGameplayAbilitySpecHandle> GrantedAbilitySpecHandles;
 };
