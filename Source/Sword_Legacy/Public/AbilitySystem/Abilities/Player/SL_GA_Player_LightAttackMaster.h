8accd62c9510644319db126286f4c93870c3e2ea
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-27 00:41:44 +0530

    Setting up for Light Attack

---
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_LightAttackMaster.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/SL_PlayerGameplayAbility.h"
+#include "SL_GA_Player_LightAttackMaster.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API USL_GA_Player_LightAttackMaster : public USL_PlayerGameplayAbility
+{
+	GENERATED_BODY()
+};
