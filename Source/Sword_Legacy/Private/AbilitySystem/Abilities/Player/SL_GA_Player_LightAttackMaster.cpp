2ed340b10fa8939f89b7da46d4867dbe855f5ac8
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-27 23:18:49 +0530

    Light Attack Combo C++ Created

--- Source/Sword_Legacy/Private/AbilitySystem/Abilities/Player/SL_GA_Player_LightAttackMaster.cpp
+++ Source/Sword_Legacy/Private/AbilitySystem/Abilities/Player/SL_GA_Player_LightAttackMaster.cpp
@@ -1,1 +1,64 @@
 #include "AbilitySystem/Abilities/Player/SL_GA_Player_LightAttackMaster.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
+USL_GA_Player_LightAttackMaster::USL_GA_Player_LightAttackMaster()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	ComboResetTime = 0.3f;
+}
+
+void USL_GA_Player_LightAttackMaster::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	const float CurrentTime = GetWorld()->GetTimeSeconds();
+	if (CurrentTime - LastAttackTime > ComboResetTime)
+	{
+		CurrentLightAttackComboCount = 1;
+	}
+	
+	TObjectPtr<UAnimMontage>* MontageToPlay = LightAttackMontagesMap.Find(CurrentLightAttackComboCount);
+	
+	if (!MontageToPlay || !(*MontageToPlay))
+	{
+		CurrentLightAttackComboCount = 1;
+		MontageToPlay = LightAttackMontagesMap.Find(CurrentLightAttackComboCount);
+		
+		if (!MontageToPlay || !(*MontageToPlay))
+		{
+			EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+			return;
+		}
+	}
+	
+	UAbilityTask_PlayMontageAndWait* PlayMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this,
+		NAME_None,
+		*MontageToPlay,
+		1.0f
+	);
+	PlayMontageTask->OnCompleted.AddDynamic(this, &USL_GA_Player_LightAttackMaster::OnMontageCompleted);
+	PlayMontageTask->OnBlendOut.AddDynamic(this, &USL_GA_Player_LightAttackMaster::OnMontageCompleted);
+	PlayMontageTask->OnInterrupted.AddDynamic(this, &USL_GA_Player_LightAttackMaster::OnMontageCompleted);
+	PlayMontageTask->OnCancelled.AddDynamic(this, &USL_GA_Player_LightAttackMaster::OnMontageCompleted);
+	PlayMontageTask->ReadyForActivation();
+	
+	if (CurrentLightAttackComboCount == LightAttackMontagesMap.Num())
+	{
+		CurrentLightAttackComboCount = 1;
+	}
+	else
+	{
+		CurrentLightAttackComboCount++;
+	}
+	
+	LastAttackTime = CurrentTime;
+}
+
+void USL_GA_Player_LightAttackMaster::OnMontageCompleted()
+{
+	LastAttackTime = GetWorld()->GetTimeSeconds();
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
--- Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_LightAttackMaster.h
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_LightAttackMaster.h
@@ -8,4 +8,30 @@ UCLASS()
 class SWORD_LEGACY_API USL_GA_Player_LightAttackMaster : public USL_PlayerGameplayAbility
 {
 	GENERATED_BODY()
+	
+public:
+	USL_GA_Player_LightAttackMaster();
+	
+protected:
+	// ~ Begin UGameplayAbility Interface
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+	// ~ End UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	TMap<int32, TObjectPtr<UAnimMontage>> LightAttackMontagesMap;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float ComboResetTime;
+	
+private:
+	UFUNCTION()
+	void OnMontageCompleted();
+	
+	int32 CurrentLightAttackComboCount = 1;
+	float LastAttackTime = 0.f;
 };
