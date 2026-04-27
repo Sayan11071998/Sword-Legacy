71064c66353c96caa219b4da1b01df407e79e2a9
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-27 23:40:05 +0530

    Heavy Attack Ability Created

---
+++ Source/Sword_Legacy/Private/AbilitySystem/Abilities/SL_GA_Player_HeavyAttackMaster.cpp
@@ -0,0 +1,6 @@
+#include "AbilitySystem/Abilities/Player/SL_GA_Player_HeavyAttackMaster.h"
+
+USL_GA_Player_HeavyAttackMaster::USL_GA_Player_HeavyAttackMaster()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
---
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_HeavyAttackMaster.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/SL_PlayerGameplayAbility.h"
+#include "SL_GA_Player_HeavyAttackMaster.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API USL_GA_Player_HeavyAttackMaster : public USL_PlayerGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	USL_GA_Player_HeavyAttackMaster();
+};
