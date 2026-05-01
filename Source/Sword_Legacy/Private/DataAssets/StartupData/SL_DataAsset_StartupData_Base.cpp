71083d27461ed07cc6162df44d43c8d6d724ea03
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-02 01:20:16 +0530

    Game Effects Added to Player

--- Source/Sword_Legacy/Private/DataAssets/StartupData/SL_DataAsset_StartupData_Base.cpp
+++ Source/Sword_Legacy/Private/DataAssets/StartupData/SL_DataAsset_StartupData_Base.cpp
@@ -9,6 +9,18 @@ void USL_DataAsset_StartupData_Base::GiveToAbilitySystemComponent(
 	
 	GrantAbilities(ActivateOnGivenAbilities, InASCToGive, ApplyLevel);
 	GrantAbilities(ReactiveAbilities, InASCToGive, ApplyLevel);
+	
+	if (!StartupGameplayEffects.IsEmpty())
+	{
+		for (const TSubclassOf<UGameplayEffect>& EffectClass : StartupGameplayEffects)
+		{
+			if (!EffectClass) continue;
+			
+			UGameplayEffect* EffectCDO = EffectClass->GetDefaultObject<UGameplayEffect>();
+			
+			InASCToGive->ApplyGameplayEffectToSelf(EffectCDO, ApplyLevel, InASCToGive->MakeEffectContext());
+		}
+	}
 }
 
 void USL_DataAsset_StartupData_Base::GrantAbilities(const TArray<TSubclassOf<USL_GameplayAbility>>& InAbilitiesToGive,
