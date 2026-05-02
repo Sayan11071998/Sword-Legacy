171a0e01418939f08137001f7ee5833a050c22de
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-02 20:42:48 +0530

    Pawn Combat Component Helper Function Created

---
+++ Source/Sword_Legacy/Public/PawnTypes/SL_PawnEnumTypes.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SL_PawnEnumTypes.generated.h"
+
+UENUM()
+enum class ESL_ConfirmType : uint8
+{
+	Yes,
+	No
+};
+
+UENUM()
+enum class ESL_ValidType : uint8
+{
+	Valid,
+	Invalid
+};
--- Source/Sword_Legacy/Public/Utilities/SL_FunctionLibrary.h
+++ Source/Sword_Legacy/Public/Utilities/SL_FunctionLibrary.h
@@ -3,16 +3,11 @@
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "PawnTypes/SL_PawnEnumTypes.h"
 #include "SL_FunctionLibrary.generated.h"
 
+class USL_PawnCombatComponent;
 class USL_AbilitySystemComponent;
-
-UENUM()
-enum class ESL_ConfirmType : uint8
-{
-	Yes,
-	No
-};
 
 UCLASS()
 class SWORD_LEGACY_API USL_FunctionLibrary : public UBlueprintFunctionLibrary
@@ -22,6 +17,7 @@ class SWORD_LEGACY_API USL_FunctionLibrary : public UBlueprintFunctionLibrary
 public:
 	static TObjectPtr<USL_AbilitySystemComponent> NativeGetASCFromActor(TObjectPtr<AActor> InActor);
 	static bool NativeDoesActorHaveTag(TObjectPtr<AActor> InActor, FGameplayTag TagToCheck);
+	static TObjectPtr<USL_PawnCombatComponent> NativeGetPawnCombatComponentFromActor(TObjectPtr<AActor> InActor);
 	
 	UFUNCTION(BlueprintCallable, Category = "Pawn|Function Library")
 	static void AddGameplayToActorIfNone(AActor* InActor, FGameplayTag TagToAdd);
@@ -31,4 +27,7 @@ public:
 	
 	UFUNCTION(BlueprintCallable, Category = "Pawn|Function Library", meta = (DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
 	static void BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, ESL_ConfirmType& OutConfirmType);
+	
+	UFUNCTION(BlueprintCallable, Category = "Pawn|Function Library", meta = (DisplayName = "Get Pawn Combat Component From Actor", ExpandEnumAsExecs = "OutValidType"))
+	static  USL_PawnCombatComponent* BP_GetPawnCombatComponentFromActor(AActor* InActor, ESL_ValidType& OutValidType);
 };
