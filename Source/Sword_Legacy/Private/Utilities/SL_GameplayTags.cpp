1a7ad97274699627e92fb9258d2381cbb80fbe9b
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-09 01:04:42 +0530

    Death Tags Created

--- Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
+++ Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
@@ -37,11 +37,14 @@ namespace SL_GameplayTags
 	
 	/** Shared Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Ability_HitReact, "Shared.Ability.HitReact");
+	UE_DEFINE_GAMEPLAY_TAG(Shared_Ability_Death, "Shared.Ability.Death");
 	
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_MeleeHit, "Shared.Event.MeleeHit");
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_HitReact, "Shared.Event.HitReact");
 	
 	UE_DEFINE_GAMEPLAY_TAG(Shared_SetByCaller_BaseDamage, "Shared.SetByCaller.BaseDamage");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Shared_Status_Death, "Shared.Status.Death");
 	
 	/** Gameplay Cues **/
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Sounds_MeleeHit_Katana, "GameplayCue.Sounds.MeleeHit.Katana");
