190e469fda92afdd49125417b9a5dee3faf04c01
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-09 20:50:33 +0530

    Death Ability Implemented

---
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Enemy/SL_GA_Enemy_DeathBase.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/SL_EnemyGameplayAbility.h"
+#include "SL_GA_Enemy_DeathBase.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API USL_GA_Enemy_DeathBase : public USL_EnemyGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	USL_GA_Enemy_DeathBase();
+	
+protected:
+	// ~ Begin UGameplayAbility Interface
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData	
+	) override;
+	// ~ End UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Death")
+	TArray<TObjectPtr<UAnimMontage>> MontagesToPlay;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Death")
+	FGameplayTag DeathSoundGameplayCueTag;
+	
+private:
+	UFUNCTION()
+	void OnDeathFinished();
+};
