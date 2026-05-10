16c86616f6d24e26022ba4b5ec79255ebe1a92e3
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-10 23:42:11 +0530

    Basic UI Classes Created

---
+++ Source/Sword_Legacy/Public/Components/UI/SL_EnemyUIComponent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/UI/SL_PawnUIComponent.h"
+#include "SL_EnemyUIComponent.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API USL_EnemyUIComponent : public USL_PawnUIComponent
+{
+	GENERATED_BODY()
+};
---
+++ Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/UI/SL_PawnUIComponent.h"
+#include "SL_PlayerUIComponent.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API USL_PlayerUIComponent : public USL_PawnUIComponent
+{
+	GENERATED_BODY()
+};
