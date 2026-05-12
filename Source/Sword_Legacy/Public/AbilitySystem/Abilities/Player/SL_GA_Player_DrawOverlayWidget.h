5a4a44f100dc04ac84723a35951fe20a33ef2108
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-13 00:11:43 +0530

    Player Overlay Widget Ability Created

---
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_DrawOverlayWidget.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/SL_PlayerGameplayAbility.h"
+#include "SL_GA_Player_DrawOverlayWidget.generated.h"
+
+UCLASS()
+class SWORD_LEGACY_API USL_GA_Player_DrawOverlayWidget : public USL_PlayerGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	// ~ Begin UGameplayAbility Interface
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+	// ~ End UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Widget")
+	TSubclassOf<UUserWidget> OverlayWidgetClass;
+};
