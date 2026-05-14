558a1839b27c263ced70a39c8653effdc368eca7
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-15 00:05:06 +0530

    Avoidance Properties Set

--- Source/Sword_Legacy/Public/Controllers/SL_AIController.h
+++ Source/Sword_Legacy/Public/Controllers/SL_AIController.h
@@ -20,6 +20,8 @@ public:
 	// ~ End IGenericTeamAgentInterface Interface
 	
 protected:
+	virtual void BeginPlay() override;
+	
 	// ~ Begin AAIController Interface
 	virtual void OnPossess(APawn* InPawn) override;
 	// ~ End AAIController Interface
@@ -35,4 +37,14 @@ protected:
 	
 	UFUNCTION()
 	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config")
+	bool bEnableDetourCrowdAvoidance = true;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config", meta = (EditCondition = "bEnableDetourCrowdAvoidance", UIMin = "1", UIMax = "4"))
+	int32 DetourCrowdAvoidanceQuality = 4;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Detour Crowd Avoidance Config", meta = (EditCondition = "bEnableDetourCrowdAvoidance"))
+	float CollisionQueryRange = 600.f;
 };
