1a2fe81b3d7f855f8531b81df791a22f9b898e1e
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-18 23:26:04 +0530

    Helper Method Created

--- Source/Sword_Legacy/Private/AnimInstances/SL_BaseAnimInstance.cpp
+++ Source/Sword_Legacy/Private/AnimInstances/SL_BaseAnimInstance.cpp
@@ -1,1 +1,12 @@
 #include "AnimInstances/SL_BaseAnimInstance.h"
+#include "Utilities/SL_FunctionLibrary.h"
+
+bool USL_BaseAnimInstance::DoesOwnerHaveTag(FGameplayTag TagToCheck) const
+{
+	if (APawn* OwningPawn = TryGetPawnOwner())
+	{
+		return USL_FunctionLibrary::NativeDoesActorHaveTag(OwningPawn, TagToCheck);
+	}
+	
+	return false;
+}
