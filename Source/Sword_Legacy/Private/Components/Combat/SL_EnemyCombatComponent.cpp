dfedba5dad3f8b55193a6c776579fe6b2eb75d12
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-20 23:41:56 +0530

    Enemy Weapon Hit Detection Working

--- Source/Sword_Legacy/Private/Components/Combat/SL_EnemyCombatComponent.cpp
+++ Source/Sword_Legacy/Private/Components/Combat/SL_EnemyCombatComponent.cpp
@@ -1,1 +1,11 @@
 #include "Components/Combat/SL_EnemyCombatComponent.h"
+
+#include "SL_DebugHelper.h"
+
+void USL_EnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
+{
+	if (HitActor)
+	{
+		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+	}
+}
--- Source/Sword_Legacy/Public/Components/Combat/SL_EnemyCombatComponent.h
+++ Source/Sword_Legacy/Public/Components/Combat/SL_EnemyCombatComponent.h
@@ -8,4 +8,9 @@ UCLASS()
 class SWORD_LEGACY_API USL_EnemyCombatComponent : public USL_PawnCombatComponent
 {
 	GENERATED_BODY()
+	
+public:
+	// ~ Begin USL_PawnCombatComponent Interface
+	virtual void OnHitTargetActor(AActor* HitActor) override;
+	// ~ End USL_PawnCombatComponent Interface
 };
