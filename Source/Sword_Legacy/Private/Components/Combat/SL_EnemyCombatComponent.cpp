ee543eed44dae1180c96d79949e8ad6038d5158f
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-05-21 00:07:51 +0530

    Working on Successful Hit

--- Source/Sword_Legacy/Private/Components/Combat/SL_EnemyCombatComponent.cpp
+++ Source/Sword_Legacy/Private/Components/Combat/SL_EnemyCombatComponent.cpp
@@ -1,11 +1,37 @@
 #include "Components/Combat/SL_EnemyCombatComponent.h"
-
-#include "SL_DebugHelper.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Utilities/SL_GameplayTags.h"
 
 void USL_EnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	if (HitActor)
+	if (OverlappedActors.Contains(HitActor)) return;
+	
+	OverlappedActors.AddUnique(HitActor);
+	
+	bool bIsValidBlock = false;
+	
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+	
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+		
+	}
+	
+	FGameplayEventData EventData;
+	EventData.Instigator = GetOwningPawn();
+	EventData.Target = HitActor;
+	
+	if (bIsValidBlock)
+	{
+		
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			SL_GameplayTags::Shared_Event_MeleeHit,
+			EventData
+		);
 	}
 }
