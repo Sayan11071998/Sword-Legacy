4becb56cb92d36059cc42a5c716dc581faa192cc
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-06 23:17:54 +0530

    Base Projectile Components Created

--- Source/Sword_Legacy/Private/Items/Projectiles/SL_ProjectileBase.cpp
+++ Source/Sword_Legacy/Private/Items/Projectiles/SL_ProjectileBase.cpp
@@ -1,6 +1,7 @@
 #include "Items/Projectiles/SL_ProjectileBase.h"
 #include "Components/BoxComponent.h"
 #include "NiagaraComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 ASL_ProjectileBase::ASL_ProjectileBase()
 {
@@ -12,6 +13,17 @@ ASL_ProjectileBase::ASL_ProjectileBase()
 	ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Block);
 	ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldDynamic, ECollisionResponse::ECR_Block);
 	ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldStatic, ECollisionResponse::ECR_Block);
+	
+	ProjectileNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("ProjectileNiagaraComponent"));
+	ProjectileNiagaraComponent->SetupAttachment(GetRootComponent());
+	
+	ProjectileMovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComp"));
+	ProjectileMovementComp->InitialSpeed = 700.f;
+	ProjectileMovementComp->MaxSpeed = 900.f;
+	ProjectileMovementComp->Velocity = FVector(1.f, 0.f, 0.f);
+	ProjectileMovementComp->ProjectileGravityScale = 0.f;
+	
+	InitialLifeSpan = 4.f;
 }
 
 void ASL_ProjectileBase::BeginPlay()
