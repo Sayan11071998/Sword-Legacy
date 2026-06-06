2debc56f5c7ee0feae8deb8a9a144690df40da9a
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-07 00:36:26 +0530

    Projectile Overlap Working

--- Source/Sword_Legacy/Private/Items/Projectiles/SL_ProjectileBase.cpp
+++ Source/Sword_Legacy/Private/Items/Projectiles/SL_ProjectileBase.cpp
@@ -3,6 +3,8 @@
 #include "NiagaraComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
+#include "SL_DebugHelper.h"
+
 ASL_ProjectileBase::ASL_ProjectileBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
@@ -31,11 +33,21 @@ ASL_ProjectileBase::ASL_ProjectileBase()
 void ASL_ProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	if (ProjectileDamagePolicy == ESL_ProjectileDamagePolicy::OnBeginOverlap)
+	{
+		ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
 }
 
 void ASL_ProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
+	if (OtherActor)
+	{
+		Debug::Print(OtherActor->GetActorNameOrLabel());
+		Destroy();
+	}
 }
 
 void ASL_ProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
