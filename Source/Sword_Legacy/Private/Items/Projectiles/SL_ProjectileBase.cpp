789be7b7dfe3eaacd7fe62c37da4c2d902a69bf8
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-06 23:14:11 +0530

    Working on Base Projectile Class

---
+++ Source/Sword_Legacy/Private/Items/Projectiles/SL_ProjectileBase.cpp
@@ -0,0 +1,20 @@
+#include "Items/Projectiles/SL_ProjectileBase.h"
+#include "Components/BoxComponent.h"
+#include "NiagaraComponent.h"
+
+ASL_ProjectileBase::ASL_ProjectileBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	ProjectileCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("ProjectileCollisionBox"));
+	SetRootComponent(ProjectileCollisionBox);
+	ProjectileCollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Block);
+	ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldDynamic, ECollisionResponse::ECR_Block);
+	ProjectileCollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldStatic, ECollisionResponse::ECR_Block);
+}
+
+void ASL_ProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/Sword_Legacy/Public/Items/Projectiles/SL_ProjectileBase.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SL_ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+class UNiagaraComponent;
+class UBoxComponent;
+
+UENUM(BlueprintType)
+enum class ESL_ProjectileDamagePolicy : uint8
+{
+	OnHit,
+	OnBeginOverlap
+};
+
+UCLASS()
+class SWORD_LEGACY_API ASL_ProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASL_ProjectileBase();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	TObjectPtr<UBoxComponent> ProjectileCollisionBox;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	TObjectPtr<UNiagaraComponent> ProjectileNiagaraComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComp;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	ESL_ProjectileDamagePolicy ProjectileDamagePolicy = ESL_ProjectileDamagePolicy::OnHit;
+};
