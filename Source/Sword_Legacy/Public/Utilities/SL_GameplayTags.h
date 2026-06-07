d9ed145fb655d6593f16348377957de951c05e10
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-08 00:21:07 +0530

    Unblockable Tag Created

--- Source/Sword_Legacy/Public/Utilities/SL_GameplayTags.h
+++ Source/Sword_Legacy/Public/Utilities/SL_GameplayTags.h
@@ -57,6 +57,7 @@ namespace SL_GameplayTags
 	
 	SWORD_LEGACY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Status_Strafing);
 	SWORD_LEGACY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Status_UnderAttack);
+	SWORD_LEGACY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Status_Unblockable);
 	
 	/** Shared Tags **/
 	SWORD_LEGACY_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Shared_Ability_HitReact);
