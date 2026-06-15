ccb2593b4bda31822a579db8ef996d0deda70183
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-15 23:53:14 +0530

    Rage Gameplay Cue Tags Created

--- Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
+++ Source/Sword_Legacy/Private/Utilities/SL_GameplayTags.cpp
@@ -93,6 +93,7 @@ namespace SL_GameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Sounds_Death_Enemy_FrostGiant, "GameplayCue.Sounds.Death.Enemy.FrostGiant");
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Effects, "GameplayCue.Effects");
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Effects_UnblockableWarning, "GameplayCue.Effects.UnblockableWarning");
+	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Effects_Rage_Activating, "GameplayCue.Effects.Rage.Activating");
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Effects_Katana, "GameplayCue.Effects.Katana");
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Effects_Katana_SuccessfulBlock, "GameplayCue.Effects.Katana.SuccessfulBlock");
 	UE_DEFINE_GAMEPLAY_TAG(GameplayCue_Effects_Katana_PerfectBlock, "GameplayCue.Effects.Katana.PerfectBlock");
