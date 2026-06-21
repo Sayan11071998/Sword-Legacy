1292886079deb9fe3b9ff59e807818b4f0ad59a3
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-21 22:34:39 +0530

    Special Light Weapon Attack Ability Implemented

---
+++ Source/Sword_Legacy/Private/AbilitySystem/Abilities/Player/SL_GA_Player_LightWeaponAbility_Katana.cpp
@@ -0,0 +1,90 @@
+#include "AbilitySystem/Abilities/Player/SL_GA_Player_LightWeaponAbility_Katana.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Components/Combat/SL_PlayerCombatComponent.h"
+#include "Utilities/SL_GameplayTags.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+USL_GA_Player_LightWeaponAbility_Katana::USL_GA_Player_LightWeaponAbility_Katana()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerExecution;
+
+	WeaponDamageLevelMultiplier = 2.0f;
+	LevelComboCount = 4;
+}
+
+void USL_GA_Player_LightWeaponAbility_Katana::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UAbilityTask_WaitGameplayEvent* WaitMeleeHitTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this,
+		MeleeHitEventTag,
+		nullptr,
+		false,
+		true
+	);
+	WaitMeleeHitTask->EventReceived.AddDynamic(this, &USL_GA_Player_LightWeaponAbility_Katana::OnMeleeHitEventReceived);
+	WaitMeleeHitTask->ReadyForActivation();
+
+	UAbilityTask_PlayMontageAndWait* PlayMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this,
+		NAME_None,
+		SpecialLightMontage,
+		1.0f,
+		NAME_None,
+		true
+	);
+	PlayMontageTask->OnCompleted.AddDynamic(this, &USL_GA_Player_LightWeaponAbility_Katana::OnMontageCompleted);
+	PlayMontageTask->OnBlendOut.AddDynamic(this, &USL_GA_Player_LightWeaponAbility_Katana::OnMontageCompleted);
+	PlayMontageTask->OnInterrupted.AddDynamic(this, &USL_GA_Player_LightWeaponAbility_Katana::OnMontageCompleted);
+	PlayMontageTask->OnCancelled.AddDynamic(this, &USL_GA_Player_LightWeaponAbility_Katana::OnMontageCompleted);
+	PlayMontageTask->ReadyForActivation();
+}
+
+void USL_GA_Player_LightWeaponAbility_Katana::OnMontageCompleted()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void USL_GA_Player_LightWeaponAbility_Katana::OnMeleeHitEventReceived(FGameplayEventData Payload)
+{
+	USL_PlayerCombatComponent* CombatComponent = GetPlayerCombatComponentFromActorInfo();
+	if (!CombatComponent) return;
+
+	const float WeaponBaseDamage = CombatComponent->GetPlayerCurrentEquippedWeaponDamageAtLevel(static_cast<float>(GetAbilityLevel()) * WeaponDamageLevelMultiplier);
+
+	const FGameplayEffectSpecHandle DamageSpecHandle = MakePlayerDamageEffectSpecHandle(
+		DamageEffectClass,
+		WeaponBaseDamage,
+		SL_GameplayTags::Player_SetByCaller_AttackType_Light,
+		LevelComboCount
+	);
+
+	AActor* TargetActor = const_cast<AActor*>(Payload.Target.Get());
+	if (!TargetActor || !DamageSpecHandle.IsValid()) return;
+
+	const FActiveGameplayEffectHandle ActiveEffectHandle = NativeApplyEffectSpecHandleToTarget(TargetActor, DamageSpecHandle);
+
+	if (ActiveEffectHandle.WasSuccessfullyApplied())
+	{
+		if (WeaponHitSoundGameplayCueTag.IsValid())
+		{
+			FGameplayEffectContextHandle ContextHandle = DamageSpecHandle.Data->GetContext();
+			K2_ExecuteGameplayCue(WeaponHitSoundGameplayCueTag, ContextHandle);
+		}
+
+		FGameplayEventData HitReactPayload;
+		HitReactPayload.Instigator = GetAvatarActorFromActorInfo();
+		HitReactPayload.Target = TargetActor;
+
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			TargetActor,
+			SL_GameplayTags::Shared_Event_HitReact,
+			HitReactPayload
+		);
+	}
+}
---
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_LightWeaponAbility_Katana.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/SL_PlayerGameplayAbility.h"
+#include "SL_GA_Player_LightWeaponAbility_Katana.generated.h"
+
+class UAnimMontage;
+class UGameplayEffect;
+
+UCLASS()
+class SWORD_LEGACY_API USL_GA_Player_LightWeaponAbility_Katana : public USL_PlayerGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	USL_GA_Player_LightWeaponAbility_Katana();
+
+protected:
+	// ~ Begin UGameplayAbility Interface
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData	
+	) override;
+	// ~ End UGameplayAbility Interface
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	TObjectPtr<UAnimMontage> SpecialLightMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	FGameplayTag MeleeHitEventTag;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	FGameplayTag WeaponHitSoundGameplayCueTag;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float WeaponDamageLevelMultiplier;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	int32 LevelComboCount;
+
+private:
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	UFUNCTION()
+	void OnMeleeHitEventReceived(FGameplayEventData Payload);
+};
