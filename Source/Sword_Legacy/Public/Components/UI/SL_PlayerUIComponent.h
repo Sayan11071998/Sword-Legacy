7abcc24e5b74c834dbb8ad0cbcba3521bb1a830d
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-22 00:30:39 +0530

    Delegate Created

--- Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
+++ Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
@@ -2,9 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/UI/SL_PawnUIComponent.h"
+#include "GameplayTagContainer.h"
 #include "SL_PlayerUIComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate, FGameplayTag, AbilityInputTag, TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial);
 
 UCLASS()
 class SWORD_LEGACY_API USL_PlayerUIComponent : public USL_PawnUIComponent
@@ -17,4 +19,7 @@ public:
 	
 	UPROPERTY(BlueprintCallable, BlueprintAssignable)
 	FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
+	
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
 };
