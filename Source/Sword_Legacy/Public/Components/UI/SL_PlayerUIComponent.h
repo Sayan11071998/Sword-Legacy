daac3578abe729afeaa0bbf2f609d9f32269ff52
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-22 23:28:57 +0530

    Special Ability Cooldown Delegate Created

--- Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
+++ Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
@@ -7,6 +7,7 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate, FGameplayTag, AbilityInputTag, TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAbilityCooldownBeginDelegate, FGameplayTag, AbilityInputTag, float, TotalCooldownTime, float, RemainingCooldownTime);
 
 UCLASS()
 class SWORD_LEGACY_API USL_PlayerUIComponent : public USL_PawnUIComponent
@@ -22,4 +23,7 @@ public:
 	
 	UPROPERTY(BlueprintCallable, BlueprintAssignable)
 	FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
+	
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnAbilityCooldownBeginDelegate OnAbilityCooldownBegin;
 };
