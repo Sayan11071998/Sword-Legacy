aabc5931f463940bcd430f91839d551548572f0e
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-28 22:04:38 +0530

    Activating Pickup Ability

--- Source/Sword_Legacy/Private/Items/Collectables/Stones/SL_StoneBase.cpp
+++ Source/Sword_Legacy/Private/Items/Collectables/Stones/SL_StoneBase.cpp
@@ -1,1 +1,13 @@
 #include "Items/Collectables/Stones/SL_StoneBase.h"
+#include "Characters/SL_PlayerCharacter.h"
+#include "AbilitySystem/SL_AbilitySystemComponent.h"
+#include "Utilities/SL_GameplayTags.h"
+
+void ASL_StoneBase::OnPickupCollisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ASL_PlayerCharacter* OverlappedPlayerCharacter = Cast<ASL_PlayerCharacter>(OtherActor))
+	{
+		OverlappedPlayerCharacter->GetCharacterAbilitySystemComponent()->TryActivateAbilityByTag(SL_GameplayTags::Player_Ability_Pickup_Stones);
+	}
+}
--- Source/Sword_Legacy/Public/Items/Collectables/Stones/SL_StoneBase.h
+++ Source/Sword_Legacy/Public/Items/Collectables/Stones/SL_StoneBase.h
@@ -8,4 +8,17 @@ UCLASS()
 class SWORD_LEGACY_API ASL_StoneBase : public ASL_PickupBase
 {
 	GENERATED_BODY()
+	
+protected:
+	// ~ Begin ASL_PickupBase Interface
+	UFUNCTION()
+	virtual void OnPickupCollisionSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult	
+	) override;
+	// ~ End ASL_PickupBase Interface
 };
