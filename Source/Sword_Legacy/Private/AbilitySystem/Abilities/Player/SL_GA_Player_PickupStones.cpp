1ad37f792acc363c1dce89dffbae25152c980a4d
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-28 23:40:20 +0530

    Logic for Collectable Stones

--- Source/Sword_Legacy/Private/AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.cpp
+++ Source/Sword_Legacy/Private/AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.cpp
@@ -1,4 +1,7 @@
 #include "AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Characters/SL_PlayerCharacter.h"
+#include "Items/Collectables/Stones/SL_StoneBase.h"
 
 USL_GA_Player_PickupStones::USL_GA_Player_PickupStones()
 {
@@ -17,4 +20,38 @@ void USL_GA_Player_PickupStones::EndAbility(const FGameplayAbilitySpecHandle Han
 	bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void USL_GA_Player_PickupStones::CollectStones()
+{
+	CollectedStones.Empty();
+	
+	TArray<FHitResult> TraceHits;
+	
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetPlayerCharacterFromActorInfo(),
+		GetPlayerCharacterFromActorInfo()->GetActorLocation(),
+		GetPlayerCharacterFromActorInfo()->GetActorLocation() + (-GetPlayerCharacterFromActorInfo()->GetActorUpVector()) * BoxTraceDistance,
+		TraceBoxSize / 2.f,
+		(-GetPlayerCharacterFromActorInfo()->GetActorUpVector()).ToOrientationRotator(),
+		StoneTraceChannel,
+		false,
+		TArray<AActor*>(),
+		bDrawPersistentDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+		TraceHits,
+		true
+	);
+	
+	for (const FHitResult& TraceHit : TraceHits)
+	{
+		if (ASL_StoneBase* FoundStone = Cast<ASL_StoneBase>(TraceHit.GetActor()))
+		{
+			CollectedStones.AddUnique(FoundStone);
+		}
+	}
+	
+	if (CollectedStones.IsEmpty())
+	{
+		CancelAbility(GetCurrentAbilitySpecHandle(), GetCurrentActorInfo(), GetCurrentActivationInfo(), true);
+	}
 }
--- Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.h
+++ Source/Sword_Legacy/Public/AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.h
@@ -4,6 +4,8 @@
 #include "AbilitySystem/Abilities/SL_PlayerGameplayAbility.h"
 #include "SL_GA_Player_PickupStones.generated.h"
 
+class ASL_StoneBase;
+
 UCLASS()
 class SWORD_LEGACY_API USL_GA_Player_PickupStones : public USL_PlayerGameplayAbility
 {
@@ -29,4 +31,23 @@ protected:
 		bool bWasCancelled
 	) override;
 	// ~ End UGameplayAbility Interface
+	
+	UFUNCTION(BlueprintCallable)
+	void CollectStones();
+	
+private:
+	UPROPERTY(EditDefaultsOnly)
+	float BoxTraceDistance = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly)
+	FVector TraceBoxSize = FVector(100.f);
+	
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TEnumAsByte<EObjectTypeQuery>> StoneTraceChannel;
+	
+	UPROPERTY(EditDefaultsOnly)
+	bool bDrawPersistentDebugShape = false;
+	
+	UPROPERTY()
+	TArray<TObjectPtr<ASL_StoneBase>> CollectedStones;
 };
