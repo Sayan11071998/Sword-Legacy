f45395da48e4a30dc24f9738a1905895139a52a0
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-06-28 20:27:38 +0530

    Working on Player Pickup Ability

---
+++ Source/Sword_Legacy/Private/AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.cpp
@@ -0,0 +1,15 @@
+#include "AbilitySystem/Abilities/Player/SL_GA_Player_PickupStones.h"
+
+void USL_GA_Player_PickupStones::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void USL_GA_Player_PickupStones::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
