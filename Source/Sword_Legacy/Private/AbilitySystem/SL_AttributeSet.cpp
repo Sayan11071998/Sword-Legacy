db26ddc76d55682d52a3b0610ccc445609f09858
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-07-06 23:00:39 +0530

    Update SL_AttributeSet.cpp

--- Source/Sword_Legacy/Private/AbilitySystem/SL_AttributeSet.cpp
+++ Source/Sword_Legacy/Private/AbilitySystem/SL_AttributeSet.cpp
@@ -18,22 +18,27 @@ USL_AttributeSet::USL_AttributeSet()
 
 void USL_AttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
 {
-	if (!CachedPawnUIInterface.IsValid())
+	AActor* TargetAvatar = Data.Target.GetAvatarActor();
+	if (!TargetAvatar)
 	{
-		CachedPawnUIInterface = TWeakInterfacePtr<ISL_PawnUIInterface>(Data.Target.GetAvatarActor());
+		return;
 	}
 	
-	checkf(CachedPawnUIInterface.IsValid(), TEXT("%s didn't implement ISL_PawnUIInterface"), *Data.Target.GetAvatarActor()->GetActorNameOrLabel());
+	if (!CachedPawnUIInterface.IsValid())
+	{
+		CachedPawnUIInterface = TWeakInterfacePtr<ISL_PawnUIInterface>(TargetAvatar);
+	}
 	
-	USL_PawnUIComponent* PawnUIComponent = CachedPawnUIInterface->GetPawnUIComponent();
-	
-	checkf(PawnUIComponent, TEXT("Couldn't extract a pawn UI component from %s"), *Data.Target.GetAvatarActor()->GetActorNameOrLabel());
+	USL_PawnUIComponent* PawnUIComponent = CachedPawnUIInterface.IsValid() ? CachedPawnUIInterface->GetPawnUIComponent() : nullptr;
 	
 	if (Data.EvaluatedData.Attribute == GetCurrentHealthAttribute())
 	{
 		const float NewCurrentHealth = FMath::Clamp(GetCurrentHealth(), 0.f, GetMaxHealth());
 		SetCurrentHealth(NewCurrentHealth);
-		PawnUIComponent->OnCurrentHealthChanged.Broadcast(GetCurrentHealth() / GetMaxHealth());
+		if (PawnUIComponent)
+		{
+			PawnUIComponent->OnCurrentHealthChanged.Broadcast(GetCurrentHealth() / GetMaxHealth());
+		}
 	}
 	
 	if (Data.EvaluatedData.Attribute == GetCurrentRageAttribute())
@@ -43,21 +48,24 @@ void USL_AttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectMod
 		
 		if (GetCurrentRage() == GetMaxRage())
 		{
-			USL_FunctionLibrary::AddGameplayTagToActorIfNone(Data.Target.GetAvatarActor(), SL_GameplayTags::Player_Status_Rage_Full);
+			USL_FunctionLibrary::AddGameplayTagToActorIfNone(TargetAvatar, SL_GameplayTags::Player_Status_Rage_Full);
 		}
 		else if (GetCurrentRage() == 0.f)
 		{
-			USL_FunctionLibrary::AddGameplayTagToActorIfNone(Data.Target.GetAvatarActor(), SL_GameplayTags::Player_Status_Rage_None);
+			USL_FunctionLibrary::AddGameplayTagToActorIfNone(TargetAvatar, SL_GameplayTags::Player_Status_Rage_None);
 		}
 		else
 		{
-			USL_FunctionLibrary::RemoveGameplayTagFromActorIfFound(Data.Target.GetAvatarActor(), SL_GameplayTags::Player_Status_Rage_Full);
-			USL_FunctionLibrary::RemoveGameplayTagFromActorIfFound(Data.Target.GetAvatarActor(), SL_GameplayTags::Player_Status_Rage_None);
+			USL_FunctionLibrary::RemoveGameplayTagFromActorIfFound(TargetAvatar, SL_GameplayTags::Player_Status_Rage_Full);
+			USL_FunctionLibrary::RemoveGameplayTagFromActorIfFound(TargetAvatar, SL_GameplayTags::Player_Status_Rage_None);
 		}
 		
-		if (USL_PlayerUIComponent* PlayerUIComponent = CachedPawnUIInterface->GetPlayerUIComponent())
+		if (CachedPawnUIInterface.IsValid())
 		{
-			PlayerUIComponent->OnCurrentRageChanged.Broadcast(GetCurrentRage() / GetMaxRage());
+			if (USL_PlayerUIComponent* PlayerUIComponent = CachedPawnUIInterface->GetPlayerUIComponent())
+			{
+				PlayerUIComponent->OnCurrentRageChanged.Broadcast(GetCurrentRage() / GetMaxRage());
+			}
 		}
 	}
 	
@@ -69,11 +77,14 @@ void USL_AttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectMod
 		
 		SetCurrentHealth(NewCurrentHealth);
 		
-		PawnUIComponent->OnCurrentHealthChanged.Broadcast(GetCurrentHealth() / GetMaxHealth());
+		if (PawnUIComponent)
+		{
+			PawnUIComponent->OnCurrentHealthChanged.Broadcast(GetCurrentHealth() / GetMaxHealth());
+		}
 		
 		if (GetCurrentHealth() == 0.f)
 		{
-			USL_FunctionLibrary::AddGameplayTagToActorIfNone(Data.Target.GetAvatarActor(), SL_GameplayTags::Shared_Status_Death);
+			USL_FunctionLibrary::AddGameplayTagToActorIfNone(TargetAvatar, SL_GameplayTags::Shared_Status_Death);
 		}
 	}
 }
