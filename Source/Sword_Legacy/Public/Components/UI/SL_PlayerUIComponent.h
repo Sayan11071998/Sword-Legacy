55177c9de1c71f68e0ee79b6aac5f11e5720b1c3
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-07-06 22:56:06 +0530

    Register/Clear Player Widget

--- Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
+++ Source/Sword_Legacy/Public/Components/UI/SL_PlayerUIComponent.h
@@ -5,6 +5,8 @@
 #include "GameplayTagContainer.h"
 #include "SL_PlayerUIComponent.generated.h"
 
+class UUserWidget;
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChangedDelegate, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAbilityIconSlotUpdatedDelegate, FGameplayTag, AbilityInputTag, TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnAbilityCooldownBeginDelegate, FGameplayTag, AbilityInputTag, float, TotalCooldownTime, float, RemainingCooldownTime);
@@ -16,18 +18,28 @@ class SWORD_LEGACY_API USL_PlayerUIComponent : public USL_PawnUIComponent
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterPlayerDrawnWidget(UUserWidget* InWidgetToRegister);
+	
+	UFUNCTION(BlueprintCallable)
+	void RemovePlayerDrawnWidgetsIfAny();
+	
 	UPROPERTY(BlueprintAssignable)
 	FOnPercentChangedDelegate OnCurrentRageChanged;
 	
-	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	UPROPERTY(BlueprintAssignable)
 	FOnEquippedWeaponChangedDelegate OnEquippedWeaponChanged;
 	
-	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	UPROPERTY(BlueprintAssignable)
 	FOnAbilityIconSlotUpdatedDelegate OnAbilityIconSlotUpdated;
 	
-	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	UPROPERTY(BlueprintAssignable)
 	FOnAbilityCooldownBeginDelegate OnAbilityCooldownBegin;
 	
-	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	UPROPERTY(BlueprintAssignable)
 	FOnStoneInteractedDelegate OnStoneInteracted;
+
+private:
+	UPROPERTY()
+	TArray<TObjectPtr<UUserWidget>> PlayerDrawnWidgets;
 };
