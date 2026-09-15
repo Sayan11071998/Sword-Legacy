cafdf24500b929403596cc022e2fd0b9412671b8
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-09-15 21:36:33 +0530

    Custom Bound Action Button Logid

--- Source/Sword_Legacy/Public/Widgets/Options/SL_Widget_OptionsScreen.h
+++ Source/Sword_Legacy/Public/Widgets/Options/SL_Widget_OptionsScreen.h
@@ -8,4 +8,19 @@ UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class SWORD_LEGACY_API USL_Widget_OptionsScreen : public USL_Widget_Activatable_Base
 {
 	GENERATED_BODY()
+	
+protected:
+	// ~ Begin UUserWidget Interface
+	virtual void NativeOnInitialized() override;
+	// ~ End UUserWidget Interface
+	
+private:
+	void OnResetBoundActionTriggered();
+	void OnBackBoundActionTriggered();
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Game Options Screen", meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+	
+	UPROPERTY()
+	FUIActionBindingHandle ResetActionHandle;
 };
