03e584f94c954168f0d8b3d83787ed84cd646130
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-09-19 00:14:13 +0530

    Creating List Entry

---
+++ Source/Sword_Legacy/Public/Widgets/Options/ListEntries/SL_Widget_ListEntry_Base.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "SL_Widget_ListEntry_Base.generated.h"
+
+class UCommonTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class SWORD_LEGACY_API USL_Widget_ListEntry_Base : public UCommonUserWidget, public IUserObjectListEntry
+{
+	GENERATED_BODY()
+	
+protected:
+	// ~ Begin IUserObjectListEntry Interface
+	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
+	// ~ End IUserObjectListEntry Inter
+	
+private:
+	// Bound Widgets
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional, AllowPrivateAccess = "true"))
+	TObjectPtr<UCommonTextBlock> CommonText_SettingsDisplayName;
+};
