fb869e714d1612a79975df3ad63abf43089cf8bf
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-09-19 23:22:01 +0530

    Generating Entry Widgets

--- Source/Sword_Legacy/Public/Widgets/Components/SL_CommonListView.h
+++ Source/Sword_Legacy/Public/Widgets/Components/SL_CommonListView.h
@@ -4,8 +4,29 @@
 #include "CommonListView.h"
 #include "SL_CommonListView.generated.h"
 
+class USL_DataAsset_DataListEntryMapping;
+
 UCLASS()
 class SWORD_LEGACY_API USL_CommonListView : public UCommonListView
 {
 	GENERATED_BODY()
+	
+protected:
+	// ~ Begin UCommonListView Interface
+	virtual UUserWidget& OnGenerateEntryWidgetInternal(
+		UObject* Item,
+		TSubclassOf<UUserWidget> DesiredEntryClass,
+		const TSharedRef<STableViewBase>& OwnerTable	
+	) override;
+	// ~ End UCommonListView Interface
+	
+private:
+	// ~ Begin UWidget Interface
+#if WITH_EDITOR	
+	virtual void ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const override;
+#endif
+	// ~ End UWidget Interface
+	
+	UPROPERTY(EditAnywhere, Category = "Game List View Settings")
+	TObjectPtr<USL_DataAsset_DataListEntryMapping> DataListEntryMapping;
 };
