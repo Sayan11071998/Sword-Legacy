a1fbfcf131aeb6f44d21a179090e5406378151cc
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-09-22 23:54:35 +0530

    Working on Options Details

---
+++ Source/Sword_Legacy/Public/Widgets/Options/SL_Widget_OptionsDetailsView.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SL_Widget_OptionsDetailsView.generated.h"
+
+class UCommonRichTextBlock;
+class UCommonLazyImage;
+class UCommonTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class SWORD_LEGACY_API USL_Widget_OptionsDetailsView : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	// Bound Widgets
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> CommonTextBlock_Title;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonLazyImage> CommonLazyImage_DescriptionImage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_Description;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_DynamicDetails;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_DisabledReason;
+};
